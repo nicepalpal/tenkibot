daef4bec2a91e53ca67f6b2a13a234d74db05c99
Andrew Pelizzaro <nicepalpal@gmail.com>
2019-03-29 17:36:07 +0900

    city list notes half way done

--- city_list.py
+++ city_list.py
@@ -7,13 +7,23 @@ def get_weather_list():
 
     #savename = 'primary_area.xml'
     #urlretrieve(url, savename)
+    #Parses over all the elementree XML elements
     tree = ElementTree.parse('primary_area.xml')
 
     #getroot() メソッドでXMLのルート要素（この例ではrss要素）に対応する
     #Elementオブジェクトを得る
+    #Getroot grabs the first element in the XML tree
     root = tree.getroot()
 
     #都市リスト:city_dictの初期化
     city_dict = {}
 
     #findall()メソッドでXPathにマッチする要素のリストを取得する
+    for pref in root.findall('.//pref'):
+
+        pref_name + pref.get('title')
+
+        for city in pref.findall('.//city'):
+            city_name = city.get('title')
+            city_id = city.get('id')
+            city_dict[city_name] = city_id
