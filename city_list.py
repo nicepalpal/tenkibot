f223745236b03b67047ef1d2cd00304be59a7845
Andrew P <nicepalpal@gmail.com>
2019-04-01 15:45:06 +0900

    completed city list

--- city_list.py
+++ city_list.py
@@ -21,9 +21,11 @@ def get_weather_list():
     #findall()メソッドでXPathにマッチする要素のリストを取得する
     for pref in root.findall('.//pref'):
 
-        pref_name + pref.get('title')
+        pref_name = pref.get('title')
 
         for city in pref.findall('.//city'):
             city_name = city.get('title')
             city_id = city.get('id')
             city_dict[city_name] = city_id
+
+    return city_dict
